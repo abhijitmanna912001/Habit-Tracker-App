ef007c8bfe9a1b5ff53c3cf5caaecb8e36001b2d
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2026-03-29 00:31:29 +0530

    Fetching & Displaying Today's Habits

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,34 @@
+import { COLLECTION_ID, databases, DB_ID } from "@/lib/appwrite";
 import { useAuth } from "@/lib/auth-context";
-import { View, Text } from "react-native";
+import { Habit } from "@/types/db.types";
+import { useCallback, useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import { Query } from "react-native-appwrite";
 import { Button } from "react-native-paper";
 
 export default function Index() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const [habits, setHabits] = useState<Habit[]>();
+
+  const fetchHabits = useCallback(async () => {
+    if (!user) return;
+
+    try {
+      const response = await databases.listDocuments({
+        databaseId: DB_ID!,
+        collectionId: COLLECTION_ID!,
+        queries: [Query.equal("user_id", user.$id)],
+      });
+
+      setHabits(response.documents as unknown as Habit[]);
+    } catch (error) {
+      console.error("Failed to fetch habits:", error);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    fetchHabits();
+  }, [fetchHabits]);
 
   return (
     <View>
