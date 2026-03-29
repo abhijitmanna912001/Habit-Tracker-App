c060f1a440b59ce5be00b1426068bd035bb06173
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2026-03-29 14:53:34 +0530

    Habits UI build

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import { COLLECTION_ID, databases, DB_ID } from "@/lib/appwrite";
 import { useAuth } from "@/lib/auth-context";
 import { Habit } from "@/types/db.types";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { useCallback, useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { Query } from "react-native-appwrite";
-import { Button } from "react-native-paper";
+import { Button, Text } from "react-native-paper";
 
 export default function Index() {
   const { signOut, user } = useAuth();
@@ -32,10 +33,41 @@ export default function Index() {
 
   return (
     <View>
-      <Text>Home Screen</Text>
-      <Button mode="text" onPress={signOut} icon={"logout"}>
-        Sign Out
-      </Button>
+      <View>
+        <Text variant="headlineSmall">Today&apos;s Habits</Text>
+        <Button mode="text" onPress={signOut} icon={"logout"}>
+          Sign Out
+        </Button>
+      </View>
+
+      {habits?.length === 0 ? (
+        <View>
+          <Text>No habits yet. Add your first habit!</Text>
+        </View>
+      ) : (
+        habits?.map((habit) => (
+          <View key={habit.$id}>
+            <Text>{habit.title}</Text>
+            <Text>{habit.description}</Text>
+            <View>
+              <View>
+                <MaterialCommunityIcons
+                  name="fire"
+                  size={18}
+                  color={"#ff9800"}
+                />
+                <Text>{habit.streak_count} day streak</Text>
+              </View>
+              <View>
+                <Text>
+                  {habit.frequency.charAt(0).toUpperCase() +
+                    habit.frequency.slice(1)}
+                </Text>
+              </View>
+            </View>
+          </View>
+        ))
+      )}
     </View>
   );
 }
